08ba7b8ad974f9de1d47fb9dcc5f67d1552abe20
Hibahalatawi <hibahapple22@gmail.com>
2022-12-11 10:04:18 +0300

    Initial Commit

---
+++ googlemap/googlemapApp.swift
@@ -0,0 +1,17 @@
+//
+//  googlemapApp.swift
+//  googlemap
+//
+//  Created by Hibah Abdullah Alatawi on 17/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct googlemapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
